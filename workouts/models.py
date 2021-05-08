11590491285e87016039116a9bbece9b46eb1d9a
Vincent Veri <vincent.veri@gmail.com>
2021-05-08 19:25:11 +0200

    handle models in admin area

--- workouts/models.py
+++ workouts/models.py
@@ -6,16 +6,21 @@ class Exercise(models.Model):
     reps = models.IntegerField()
     weight = models.DecimalField(default=0, max_digits=5, decimal_places=2)
 
+    def __str__(self):
+        return self.description
 
 class Wod(models.Model):
     wod_date = models.DateField('WOD Date')
-    description = models.CharField(max_length=255)
-    duration = models.IntegerField()
+    description = models.CharField(max_length=255, null=True)
+    duration = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "{}".format(self.wod_date)
 
 
 class WodExercise(models.Model):
     wod = models.ForeignKey(Wod, on_delete=models.CASCADE)
     exercise = models.ForeignKey(Exercise, on_delete=models.SET_NULL, null=True)
-    rounds = models.JSONField()
-    duration = models.IntegerField()
-    notes = models.TextField()
+    rounds = models.JSONField(null=True)
+    duration = models.IntegerField(default=0)
+    notes = models.TextField(null=True)

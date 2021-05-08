7cdfe40f443055796d41711c7b1b9cba6215e5c6
Vincent Veri <vincent.veri@gmail.com>
2021-05-09 00:00:57 +0200

    use bulma.io templates

--- workouts/views.py
+++ workouts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-def index():
-    return HttpResponse("Welcome to lggr")
+def index(request):
+    return render(request, 'base.html')

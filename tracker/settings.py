7e40170e7d503e4468693dd91d025f32c749fe31
Vincent Veri <vincent.veri@gmail.com>
2021-05-08 23:48:11 +0200

    new view function in workouts, changed some settings

--- tracker/settings.py
+++ tracker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u-8-)@2%xr3!y9e($vei8b(flda!#4uc1-kqh^hbtck*l1b14('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'lggr.herokuapp.com']
 
--- tracker/urls.py
+++ tracker/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import workouts.views
 
 urlpatterns = [
+#    path('', workouts.views.index),
     path('admin/', admin.site.urls),
 ]
